d2c4e3196a1343eaed244fab1efa0e1ac6c7f43d
user1 <60358348+awevgsf@users.noreply.github.com>
2025-02-15 00:25:22 +0100

    Dark Mode Added

--- Pages/PoemDetail.tsx
+++ Pages/PoemDetail.tsx
@@ -1,121 +1,211 @@
 import React, { useEffect, useState } from "react";
-import { ScrollView, Text, StyleSheet, View, TouchableHighlight } from 'react-native';
+import {
+  ScrollView,
+  Text,
+  StyleSheet,
+  View,
+  TouchableHighlight,
+  useColorScheme,
+} from "react-native";
 import { Fonts } from "../android/app/src/constants/fonts";
-const PoemDetail = ({ route, navigation }: { route: any, navigation: any }) => {
-    const PoemText = route.params['text'];
-    const PoemTitle = route.params['title'];
-    const [fontSize, setfontSize] = useState(styles.PoemText.fontSize)
 
-    const addFontSize = () => {
-        if (fontSize <= 25) {
-            setfontSize(fontSize => fontSize + 1);
-        }
+const PoemDetail = ({
+  route,
+  navigation,
+}: {
+  route: any;
+  navigation: any;
+}) => {
+  const colorScheme = useColorScheme();
+  const PoemText = route.params["text"];
+  const PoemTitle = route.params["title"];
+  const [fontSize, setfontSize] = useState(styles.PoemText.fontSize);
+
+  const addFontSize = () => {
+    if (fontSize <= 25) {
+      setfontSize((fontSize) => fontSize + 1);
     }
+  };
 
-    const reduceFontSize = () => {
-        if (fontSize >= 15) {
-            setfontSize(fontSize - 1);
-        }
+  const reduceFontSize = () => {
+    if (fontSize >= 15) {
+      setfontSize(fontSize - 1);
     }
+  };
 
-    useEffect(() => {
-        navigation.setOptions({
-            title: PoemTitle,
-            headerTitleStyle: {
-                fontFamily: Fonts.NotoSerif.Regular,
+  useEffect(() => {
+    navigation.setOptions({
+      title: PoemTitle,
+      headerTitleStyle: {
+        fontFamily: Fonts.NotoSerif.Regular,
+        color: colorScheme === "light" ? "black" : "white",
+      },
+      headerTintColor: colorScheme === "light" ? "black" : "white",
+      headerStyle:{
+          backgroundColor: colorScheme === "light" ? "white" : "#121212",
+      },
+    });
+  }, [navigation, PoemTitle, colorScheme]);
+
+  return (
+    <View
+      style={
+        colorScheme === "light" ? styles.container : styles.darkContainer
+      }
+    >
+      <View style={styles.buttonContainer}>
+        <TouchableHighlight
+          style={
+            colorScheme === "light"
+              ? styles.fontSizeButton
+              : styles.darkFontSizeButton
+          }
+          activeOpacity={0.6}
+          underlayColor={colorScheme === "light" ? "white" : "#333333"}
+          onPress={reduceFontSize}
+        >
+          <Text
+            style={
+              colorScheme === "light"
+                ? styles.buttonText
+                : styles.darkButtonText
             }
-        });
-    }, [navigation, PoemTitle]);
+          >
+            Font Size -
+          </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={
+            colorScheme === "light"
+              ? styles.fontSizeButton
+              : styles.darkFontSizeButton
+          }
+          activeOpacity={0.6}
+          underlayColor={colorScheme === "light" ? "white" : "#333333"}
+          onPress={addFontSize}
+        >
+          <Text
+            style={
+              colorScheme === "light"
+                ? styles.buttonText
+                : styles.darkButtonText
+            }
+          >
+            Font Size +
+          </Text>
+        </TouchableHighlight>
+      </View>
 
-
-
-    return (
-        <View style={{flex:1}}>
-
-            <View style={styles.buttonContainer}>
-                <TouchableHighlight
-                    style={styles.fontSizeButton}
-                    activeOpacity={0.6}
-                    underlayColor={'white'}
-                    onPress={reduceFontSize}
-                >
-                    <Text style={styles.buttonText}>
-                        Font Size -
-                    </Text>
-                </TouchableHighlight>
-                <TouchableHighlight
-                    style={styles.fontSizeButton}
-                    activeOpacity={0.6}
-                    underlayColor={'white'}
-                    onPress={addFontSize}
-                >
-                    <Text style={styles.buttonText}>
-                        Font Size +
-                    </Text>
-                </TouchableHighlight>
-            </View>
-
-            <ScrollView contentContainerStyle={styles.PoemTextContainer}>
-                {PoemText.split("\n").map((line: string, index: number) => (
-                    <Text style={[styles.PoemText, { fontSize }]}>{line}{"\n"}</Text>
-                ))}
-            </ScrollView>
-        </View>
-    );
+      <ScrollView
+        contentContainerStyle={
+          colorScheme === "light"
+            ? styles.PoemTextContainer
+            : styles.darkPoemTextContainer
+        }
+      >
+        {PoemText.split("\n").map((line: string, index: number) => (
+          <Text
+            key={index}
+            style={[
+              colorScheme === "light"
+                ? styles.PoemText
+                : styles.darkPoemText,
+              { fontSize },
+            ]}
+          >
+            {line}
+            {"\n"}
+          </Text>
+        ))}
+      </ScrollView>
+    </View>
+  );
 };
 
-
 const styles = StyleSheet.create({
-    PoemText: {
-        fontSize: 19,
-        color: 'black',
-        fontFamily: Fonts.NotoSerif.Regular,
-        textAlign: 'center',
-        lineHeight: 26,
-    },
-    PoemTextContainer: {
-        backgroundColor: '#FFFFFF',
-        padding: 20,
-        // marginVertical: 8,
-        // marginHorizontal: 10,
-        // borderRadius: 15,
-        // elevation: 5,
-        // shadowColor: "#000",
-        shadowOffset: { width: 0, height: 5 },
-
-    },
-    fontSizeButton: {
-        flex: 1,
-        height: 40,
-        fontSize: 19,
-        padding: 5,
-        borderRadius: 5,
-        alignItems: 'center',
-        marginTop: 10,
-        marginBottom: 3,
-        backgroundColor: 'white',
-        justifyContent: 'center',
-        marginHorizontal: 5,
-        elevation: 5,
-    },
-    buttonContainer: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-
-        marginBottom: 8,
-        gap: 0,
-        paddingLeft: 10,
-        paddingRight: 10,
-    },
-    buttonText: {
-        color: 'black',
-        fontSize: 17,
-        fontFamily:Fonts.NotoSerif.Regular,
-        textAlign: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+  },
+  darkContainer: {
+    flex: 1,
+    backgroundColor: "#121212",
+  },
+  PoemText: {
+    fontSize: 19,
+    color: "black",
+    fontFamily: Fonts.NotoSerif.Regular,
+    textAlign: "center",
+    lineHeight: 26,
+  },
+  darkPoemText: {
+    fontSize: 19,
+    color: "white",
+    fontFamily: Fonts.NotoSerif.Regular,
+    textAlign: "center",
+    lineHeight: 26,
+  },
+  PoemTextContainer: {
+    backgroundColor: "#FFFFFF",
+    padding: 20,
+    shadowOffset: { width: 0, height: 5 },
+    flexGrow: 1,
+  },
+  darkPoemTextContainer: {
+    backgroundColor: "#121212",
+    padding: 20,
+    shadowOffset: { width: 0, height: 5 },
+    flexGrow: 1,
+  },
+  fontSizeButton: {
+    flex: 1,
+    height: 40,
+    fontSize: 19,
+    padding: 5,
+    borderRadius: 5,
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 3,
+    backgroundColor: "white",
+    justifyContent: "center",
+    marginHorizontal: 5,
+    elevation: 5,
+  },
+  darkFontSizeButton: {
+    flex: 1,
+    height: 40,
+    fontSize: 19,
+    padding: 5,
+    borderRadius: 5,
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 3,
+    backgroundColor: "#1f1f1f",
+    justifyContent: "center",
+    marginHorizontal: 5,
+    elevation: 5,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 8,
+    gap: 0,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  buttonText: {
+    color: "black",
+    fontSize: 17,
+    fontFamily: Fonts.NotoSerif.Regular,
+    textAlign: "center",
+  },
+  darkButtonText: {
+    color: "white",
+    fontSize: 17,
+    fontFamily: Fonts.NotoSerif.Regular,
+    textAlign: "center",
+  },
 });
-
-
 
 export default PoemDetail;
--- Pages/Poems.tsx
+++ Pages/Poems.tsx
@@ -1,60 +1,60 @@
-import React, { useState , useEffect} from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableHighlight } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, TouchableHighlight, useColorScheme } from 'react-native';
 import { faker } from '@faker-js/faker';
-import { Button, SearchBar } from 'react-native-elements'
+import { SearchBar } from 'react-native-elements';
 import PoemsData from '../Poems.json';
 import { Fonts } from "../android/app/src/constants/fonts";
+
 const generateDummyPoems = () => (
   Array.from({ length: 20 }, (_, index) => ({
     id: index + 1,
     title: faker.lorem.words(5),
-    Poem: faker.lorem.paragraphs(3),
+    poem: faker.lorem.paragraphs(3),
     author: faker.person.fullName(),
   }))
 );
 
-// const PoemsData = generateDummyPoems();
-
-// const ITEMS_PER_PAGE = 10;
-
-const Poems = ({ navigation , route }: { navigation: any , route:any}) => {
-  useEffect(()=>{
+const Poems = ({ navigation, route }: { navigation: any, route: any }) => {
+  const colorScheme = useColorScheme();
+  useEffect(() => {
     navigation.setOptions({
-      title:route.name,
-      headerTitleStyle:{
-        fontFamily:Fonts.NotoSerif.Regular,
+      title: route.name,
+      headerTitleStyle: {
+        fontFamily: Fonts.NotoSerif.Regular,
+        color: colorScheme === 'light' ? 'black' : 'white',
+      },
+      headerTintColor: colorScheme === "light" ? "black" : "white",
+      headerStyle: {
+        backgroundColor: colorScheme === 'light' ? '#f0f0f0' : '#121212',
       },
     });
-  }, [navigation,route]);
+  }, [navigation, route, colorScheme]);
 
-  const [searchQuery, setsearchQuery] = useState('');
-  const [filteredPoems, setfilteredPoems] = useState(PoemsData);
-  // const [currentPage, setCurrentPage] = useState(1);
-  // const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredPoems, setFilteredPoems] = useState(PoemsData);
 
   const handleSearch = (query: string): void => {
-    setsearchQuery(query);
+    setSearchQuery(query);
     if (query === '') {
-      setfilteredPoems(PoemsData);
-    }
-    else {
+      setFilteredPoems(PoemsData);
+    } else {
       const filtered = PoemsData.filter(Poem =>
         Poem.title.toLowerCase().includes(query.toLowerCase()) ||
         Poem.author.toLowerCase().includes(query.toLowerCase()) ||
         Poem.poem.toLowerCase().includes(query.toLowerCase())
       );
-      setfilteredPoems(filtered);
+      setFilteredPoems(filtered);
     }
   };
 
   return (
-    <View style={styles.container}>
+    <View style={colorScheme === 'light' ? styles.container : styles.darkContainer}>
       <SearchBar
         placeholder='    Search Poems...'
-        placeholderTextColor="black"
-        containerStyle={styles.searchContainer}
-        inputContainerStyle={styles.searchInputContainer}
-        inputStyle={styles.searchInput}
+        placeholderTextColor={colorScheme === 'light' ? "black" : "white"}
+        containerStyle={colorScheme === 'light' ? styles.searchContainer : styles.darkSearchContainer}
+        inputContainerStyle={colorScheme === 'light' ? styles.searchInputContainer : styles.darkSearchInputContainer}
+        inputStyle={colorScheme === 'light' ? styles.searchInput : styles.darkSearchInput}
         value={searchQuery}
         onChangeText={handleSearch}
         searchIcon={false}
@@ -63,50 +63,65 @@ const Poems = ({ navigation , route }: { navigation: any , route:any}) => {
         data={filteredPoems}
         renderItem={({ item }) => (
           <TouchableHighlight
-            onLongPress={()=>{}}
+            onLongPress={() => { }}
             onPress={() => navigation.navigate('PoemDetail', { text: item.poem, title: item.title })}
-            style={styles.PoemItem}
-            underlayColor={"#d3d3d3"}
+            style={colorScheme === 'light' ? styles.PoemItem : styles.darkPoemItem}
+            underlayColor={colorScheme === 'light' ? "#d3d3d3" : "#333333"}
             activeOpacity={0.6}
           >
             <View>
-              <Text style={styles.PoemTitleText}>{item.title}</Text>
-              <Text style={styles.PoemAuthorText}>{item.author}</Text>
+              <Text style={colorScheme === 'light' ? styles.PoemTitleText : styles.darkPoemTitleText}>{item.title}</Text>
+              <Text style={colorScheme === 'light' ? styles.PoemAuthorText : styles.darkPoemAuthorText}>{item.author}</Text>
               <Text>{"\n"}</Text>
             </View>
           </TouchableHighlight>
         )}
         keyExtractor={(item, index) => index.toString()}
-      ></FlatList>
+      />
     </View>
   );
 };
-
-
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f0f0f0',
   },
+  darkContainer: {
+    flex: 1,
+    backgroundColor: '#121212',
+  },
   searchContainer: {
     backgroundColor: '#f0f0f0',
     borderBottomWidth: 0,
     borderTopWidth: 0,
-    marginTop:10,
+    marginTop: 10,
+    padding: 10,
+  },
+  darkSearchContainer: {
+    backgroundColor: '#121212',
+    borderBottomWidth: 0,
+    borderTopWidth: 0,
+    marginTop: 10,
     padding: 10,
   },
   searchInputContainer: {
     backgroundColor: '#fff',
     borderRadius: 30,
   },
+  darkSearchInputContainer: {
+    backgroundColor: '#1f1f1f',
+    borderRadius: 30,
+  },
   searchInput: {
     fontFamily: Fonts.NotoSerif.Regular,
     color: '#000',
   },
-  PoemItem: {
+  darkSearchInput: {
     fontFamily: Fonts.NotoSerif.Regular,
+    color: '#fff',
+  },
+  PoemItem: {
     backgroundColor: '#fff',
     padding: 15,
     marginVertical: 8,
@@ -114,19 +129,41 @@ const styles = StyleSheet.create({
     borderRadius: 9,
     elevation: 5,
   },
-  PoemTitleText: { 
-    fontFamily: Fonts.NotoSerif.Regular,
-    fontSize: 20, 
-    paddingLeft: 10, 
-    paddingRight: 10, 
-    color: 'black', 
+  darkPoemItem: {
+    backgroundColor: '#1f1f1f',
+    padding: 15,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 9,
+    elevation: 5,
   },
-  PoemAuthorText:{ 
+  PoemTitleText: {
     fontFamily: Fonts.NotoSerif.Regular,
-    fontSize: 14, 
-    paddingLeft: 10, 
-    paddingRight: 10, 
-    color: 'black', 
+    fontSize: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    color: 'black',
+  },
+  darkPoemTitleText: {
+    fontFamily: Fonts.NotoSerif.Regular,
+    fontSize: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    color: 'white',
+  },
+  PoemAuthorText: {
+    fontFamily: Fonts.NotoSerif.Regular,
+    fontSize: 14,
+    paddingLeft: 10,
+    paddingRight: 10,
+    color: 'black',
+  },
+  darkPoemAuthorText: {
+    fontFamily: Fonts.NotoSerif.Regular,
+    fontSize: 14,
+    paddingLeft: 10,
+    paddingRight: 10,
+    color: 'white',
   },
 });
 
